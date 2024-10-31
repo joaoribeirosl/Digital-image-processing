cefcae9ded332e7b3b422b5d43a07a981a8c8c85
João Ribeiro <joaoribeiro8699@gmail.com>
2024-10-31 19:46:46 -0300

    feat: refactor exercise 1 + add exercise 2

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Digital Image Processing exercices
 
 ![exercise_1](./doc/exercise1.png)
+
+`python .\cameraman.py` to run the exercise 1 code.
+
+![exercise_2](./doc/exercise2.png)
+
+`python .\tools.py` to run the exercise 2 code
---
+++ tools.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+from pathlib import Path
+
+image_path = Path('assets', 'tools.bmp')
+image = cv2.imread(image_path.as_posix(), cv2.IMREAD_GRAYSCALE)
+
+_, thresh = cv2.threshold(image, 225, 255, cv2.THRESH_BINARY_INV)
+kernal = np.ones((2, 2), np.uint8)
+
+dilation = cv2.dilate(thresh, kernal, iterations=2)
+
+contours, hierarchy = cv2.findContours(
+    dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+tools = str(len(contours))
+print(f'{tools} tools detected in image' )
+
+cv2.imshow('Dilation', dilation)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
